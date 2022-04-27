532242fe8b8d84d3ee473099612fb7adcea4fc5c
Tufahel <tufahel96@gmail.com>
2022-04-28 03:38:17 +0600

    data.sql updated

--- schema.sql
+++ schema.sql
@@ -1,7 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT,
+    name varchar(100)
+);
+
+CREATE TABLE animals (
+    id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(300),
     date_of_birth DATE,
     escape_attempts INT,
