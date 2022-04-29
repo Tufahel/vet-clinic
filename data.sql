2f849e8820af09eed08faeeb05b9343a958fa0c8
Tufahel <tufahel96@gmail.com>
2022-04-30 05:16:58 +0600

    data.sql transaction and insertaion done

--- data.sql
+++ data.sql
@@ -16,3 +16,43 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon', 'Jun 7, 2005', 7, TRUE, 20.4);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', 'Oct 13, 1998', 3, TRUE, 17);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Ditto', 'May 14, 2022', 4, TRUE, 22);
+
+INSERT INTO owners ( full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners ( full_name, age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners ( full_name, age) VALUES ('Bob', 45);
+INSERT INTO owners ( full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners ( full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners ( full_name, age) VALUES ('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
+
+BEGIN;
+UPDATE animals
+SET species_id = (SELECT id FROM species WHERE name = 'Digimon')
+WHERE name LIKE '%mon';
+UPDATE animals
+SET species_id = (SELECT id FROM species WHERE name = 'Pokemon')
+WHERE name NOT LIKE '%mon';
+SELECT * FROM animals;
+COMMIT;
+
+BEGIN;
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+WHERE name LIKE 'Agumon'; 
+UPDATE animals
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')                                          
+WHERE name = 'Gabumon' OR name = 'Pikachu';
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob')
+WHERE name = 'Plantmon' OR name = 'Devimon';
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester')                                         
+WHERE name = 'Angemon' OR name = 'Boarmon';
+SELECT * FROM animals;
+COMMIT;
